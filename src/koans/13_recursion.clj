0d109cd520537fea2ce669773e8a6919b93e0aca
Brian McDevitt <brian.p.mcdevitt@gmail.com>
2012-08-23 13:27:07 -0500

    Finished koans. The recursive answers pass, but are not fully 'correct'.

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,28 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      false
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [c (rest coll)
+         acc (conj () (first coll))]
+    (if (empty? c)
+      acc
+      (recur (rest c) (conj acc (first c))))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if (= n 0)
+      acc
+      (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
